dc4720a80b9c6578b2ba4437d4fcdba13dc5838a
William <wescu001@fiu.edu>
2018-11-19 20:18:53 -0500

    Adding factory pattern class for services

---
+++ services/Factory.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @copyright (c) 2018 William Escudero.
+ * @package yii2-beutils
+ * @version 1.0.0
+ */
+namespace yii\beutils\services;
+
+
+/**
+ *
+ * 
+ * 
+ *
+ * @package yii\beutils\services
+ */
+class Factory 
+{
+}
